525af346449c606b3d3ac125e1bad75013e0929b
tiger1103 <yxh1103@qq.com>
2016-12-23 22:27:31 +0800

    后台登陆页面添加表单验证

--- application/admin/config.php
+++ application/admin/config.php
@@ -15,4 +15,29 @@ return [
         '__JS__'     => BASE_URL_PATH . '/static/admin/js',
     ],
 
+    //验证码配置
+    'captcha_config'=>[
+        // 验证码加密密钥
+        'seKey'    => 'yixiaohu',
+        // 验证码过期时间（s）
+        'expire'   => 1800,
+        // 验证码字体大小(px)
+        'fontSize' => 18,
+        // 是否画混淆曲线
+        'useCurve' => true,
+        // 是否添加杂点
+        'useNoise' => true,
+        // 验证码图片高度
+        'imageH'   => 40,
+        // 验证码图片宽度
+        'imageW'   => 120,
+        // 验证码位数
+        'length'   => 4,
+        // 验证码字体，不设置随机获取
+        'fontttf'  => '6.ttf',
+        // 背景颜色
+        'bg'       => [243, 251, 254],
+        // 验证成功后是否重置
+        'reset'    => true,
+    ]
 ];
--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -5,6 +5,8 @@ namespace app\admin\controller;
  * 后台默认控制器
  */
 use think\Controller;
+use think\Request;
+use think\Response;
 
 class Index extends Controller
 {
@@ -18,10 +20,18 @@ class Index extends Controller
     }
 
     public function login(){
+        if(Request::instance()->isPost()){
+            $data = ['status'=>1,'mess'=>'登陆成功。正在跳转到系统后台首页...'];//返回数据
+            $captcha = Request::instance()->post('captcha');
+            if(!captcha_check($captcha, "adminLogin", config('captcha_config'))){
+                $data = ['status'=>0,'mess'=>'验证码输入错误'];
+            }
+            return Response::create($data, 'json',200,[],[]);
+        }
         return $this->fetch();
     }
 
-    public function test(){
-        return captcha('adminLogin',config('captcha_config'));
+    public function getCaptcheUrl(){
+        return captcha("adminLogin", config('captcha_config'));
     }
 }
